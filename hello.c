951702798c5d959be33c05e5962bb6fe2f1d4e07
Jack <jack0009qq@gmail.com>
2023-10-04 16:09:40 +0800

    alothello

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    int a = 0;
+    
+    printf("Hello world\n");
+    printf("Hello world\n");
+    printf("Hello world\n");
+    printf("Hello world\n");
     printf("Hello world\n");
     return 0;
 }
